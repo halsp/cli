0bd01cdd1c6adce8ec469f82728438838a1cbbff
hal-wang <hi@hal.wang>
2022-05-24 20:11:03 +0800

    feat: add CreateCommandOptions

--- packages/cli/src/commands/create.command.ts
+++ packages/cli/src/commands/create.command.ts
@@ -2,6 +2,7 @@ import { Command } from "commander";
 import { CliStartup } from "../cli-startup";
 import { CreateMiddleware } from "../middlewares/create-middleware";
 import { BaseCommand } from "./base.command";
+import "./base-create";
 
 export class CreateCommand extends BaseCommand {
   register(command: Command): void {
@@ -9,6 +10,7 @@ export class CreateCommand extends BaseCommand {
       .command("create <name>")
       .alias("c")
       .description("Generate sfa application")
+      .setCreateOptions()
       .action(
         async (name: string, command: Record<string, boolean | string>) => {
           await new CliStartup({ name }, command).add(CreateMiddleware).run();
