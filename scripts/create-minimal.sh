020e0b28939c2cf2ef24a4e2b7124ae11a3dbc51
hal-wang <hi@hal.wang>
2022-06-17 09:41:02 +0800

    ci: add create-minimal workflow

---
+++ scripts/create-minimal.sh
@@ -0,0 +1,15 @@
+set -e
+
+npm install
+cd src/enter && npm install && cd ../..
+npm run build
+
+node bin/index.js create minimal -e http -pm npm -ps inject,router,view,mva,pipe,filter,testing,static,swagger,jwt -f
+cd minimal
+npx sfa build
+
+git config --global user.email hi@hal.wang
+git config --global user.name hal-wang
+git init -b main
+git add -A
+git commit -m "create"
