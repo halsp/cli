9eea768de949d158fecc9a5d3db00f7b248f14bb
hal-wang <hi@hal.wang>
2022-06-22 22:31:23 +0800

    test: add compiler test

--- test/services/compiler.service.test.ts
+++ test/services/compiler.service.test.ts
@@ -2,22 +2,27 @@ import { CompilerService } from "../../src/services/compiler.service";
 import { runTest } from "./runTest";
 import * as fs from "fs";
 import { WatchCompilerService } from "../../src/services/watch-compiler.service";
+import { COMMAND_TYPE_METADATA } from "../../src/constant";
 
 runTest(CompilerService, async (ctx, service) => {
   fs.rmSync("./dist-compiler", {
     recursive: true,
     force: true,
   });
+
+  let done = true;
   try {
-    const compilerResult = service.compiler("dist-compiler");
+    const compilerResult = service.compile("dist-compiler");
     expect(compilerResult).toBe(true);
     expect(fs.existsSync("./dist-compiler")).toBe(true);
+    done = true;
   } finally {
     fs.rmSync("./dist-compiler", {
       recursive: true,
       force: true,
     });
   }
+  expect(done).toBeTruthy();
 });
 
 runTest(WatchCompilerService, async (ctx, service) => {
@@ -27,3 +32,22 @@ runTest(WatchCompilerService, async (ctx, service) => {
   const newFn = (service as any).createDiagnosticReporter.bind(service)(fn);
   expect(newFn()).toEqual({ a: 1 });
 });
+
+function runCompilerOptions(command: "start" | "build") {
+  runTest(
+    CompilerService,
+    async (ctx, service) => {
+      ctx[COMMAND_TYPE_METADATA] = command;
+      expect((service as any).getCompilerOptions({}, "")).toEqual({
+        outDir: "",
+        sourceMap: command == "start",
+      });
+    },
+    undefined,
+    {
+      sourceMap: false,
+    }
+  );
+}
+runCompilerOptions("start");
+runCompilerOptions("build");
