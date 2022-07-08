49248ed5685b8c1e1ba12d33834d1039db54e4e1
hal-wang <hi@hal.wang>
2022-07-08 17:49:24 +0800

    chore: update deps

--- env/alifunc.ts
+++ env/alifunc.ts
@@ -1,9 +1,9 @@
 // 3.阿里云函数计算
 
-import { SfaAlifunc } from "@sfajs/alifunc";
+import { AlifuncStartup } from "@sfajs/alifunc";
 import startup from "./startup";
 
-const app = startup(new SfaAlifunc());
+const app = startup(new AlifuncStartup());
 export const handler = async function (req: any, resp: any, context: any) {
   await app.run(req, resp, context);
 };
--- env/http.ts
+++ env/http.ts
@@ -1,9 +1,9 @@
 // 2.原生 NodeJS 服务
 
-import { SfaHttp } from "@sfajs/http";
+import { HttpStartup } from "@sfajs/http";
 import startup from "./startup";
 
-const app = startup(new SfaHttp());
+const app = startup(new HttpStartup());
 async function bootstrap() {
   const listen = await app.dynamicListen(2333);
   console.log(`start: http://localhost:${listen.port}`);
