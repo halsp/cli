5d5d3c9df1a41a4a21e67221327254fdc35fd278
hal-wang <hi@hal.wang>
2022-07-08 13:08:48 +0800

    docs: Add README for create-sfa

--- scripts/create-sfa.ts
+++ scripts/create-sfa.ts
@@ -1,16 +1,37 @@
 import * as fs from "fs";
 import prettier from "prettier";
 
-const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
-pkg.name = "create-sfa";
-pkg.description = "sfajs 快速开始脚手架工具，由 @sfajs/cli 自动生成";
-pkg.dependencies["@sfajs/cli"] = pkg.version;
+function editPackage() {
+  const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
+  pkg.name = "create-sfa";
+  pkg.description = "sfajs 快速开始脚手架工具，由 @sfajs/cli 自动生成";
+  pkg.dependencies["@sfajs/cli"] = pkg.version;
 
-fs.renameSync("./package.json", "./package.bak.json");
+  fs.renameSync("./package.json", "./package.bak.json");
 
-fs.writeFileSync(
-  "./package.json",
-  prettier.format(JSON.stringify(pkg), {
-    parser: "json",
-  })
-);
+  fs.writeFileSync(
+    "./package.json",
+    prettier.format(JSON.stringify(pkg), {
+      parser: "json",
+    })
+  );
+}
+
+function editReadme() {
+  let readme = fs.readFileSync("./README.md", "utf-8");
+  readme = readme.replace(
+    /\<\!\-\-introduce start\-\-\>[\s\S]*\<\!\-\-introduce end\-\-\>/,
+    ""
+  );
+  readme = readme.replace(/<\!\-\-create\-sfa/, "");
+  readme = readme.replace(/create\-sfa\-\-\>/, "");
+  fs.writeFileSync(
+    "./README.md",
+    prettier.format(readme, {
+      parser: "markdown",
+    })
+  );
+}
+
+editPackage();
+editReadme();
