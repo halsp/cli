8a5ec85ff288f5c80e99cb9cb026007323ff7234
hal-wang <hi@hal.wang>
2022-07-08 12:56:00 +0800

    chore: update create-sfa deps

--- packages/cli/scripts/create-sfa.ts
+++ packages/cli/scripts/create-sfa.ts
@@ -3,7 +3,8 @@ import prettier from "prettier";
 
 const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
 pkg.name = "create-sfa";
-pkg.description = "sfajs 快速开始脚手架工具";
+pkg.description = "sfajs 快速开始脚手架工具，由 @sfajs/cli 自动生成";
+pkg.dependencies["@sfajs/cli"] = pkg.version;
 
 fs.renameSync("./package.json", "./package.bak.json");
 
