d37a0bb45fa127eab5d1b5798b303a09858227aa
hal-wang <hi@hal.wang>
2022-07-31 12:06:29 +0800

    chore: add commitlint & husky

---
+++ .commitlintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "release",
+        "template",
+        "env",
+        "build",
+        "create",
+        "info",
+        "init",
+        "start",
+        "template",
+        "update",
+      ],
+    ],
+  },
+};
