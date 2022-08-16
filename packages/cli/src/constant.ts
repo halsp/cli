ad6d236bce22e48d6601bad1391a5c5fd025fe5c
hal-wang <hi@hal.wang>
2022-08-16 17:14:16 +0800

    refactor: remove Exception hook

--- packages/cli/src/constant.ts
+++ packages/cli/src/constant.ts
@@ -1,4 +1,1 @@
-export const COMMAND_ARGS_METADATA = "@ipare/cli/commandArgs";
-export const COMMAND_OPTIONS_METADATA = "@ipare/cli/commandOptions";
-export const HOOK_EXCEPTION = "@ipare/cli/hookException";
 export const START_DEV_FILE_NAME = "__DEV_MAIN__.js";
