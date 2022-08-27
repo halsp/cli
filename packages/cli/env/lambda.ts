2125cf8e5b0f56e91598c0e2c0d17b9b24723d0a
hal-wang <hi@hal.wang>
2022-08-27 20:07:02 +0800

    chore: update env

--- packages/cli/env/lambda.ts
+++ packages/cli/env/lambda.ts
@@ -4,6 +4,4 @@ import { LambdaStartup } from "@ipare/lambda";
 import startup from "./startup";
 
 const app = startup(new LambdaStartup(), "production");
-export const main = async (event: any, context: any): Promise<unknown> => {
-  return await app.run(event, context);
-};
+export const main = (event: any, context: any) => app.run(event, context);
