399d923a631701d0b96668275ddb6439a8fb34a6
hal-wang <hi@hal.wang>
2022-12-03 20:39:15 +0800

    fix(template): version

--- template/src/startup.ts
+++ template/src/startup.ts
@@ -11,7 +11,7 @@ import "@ipare/env";
 import "@ipare/logger";
 import { HttpStartup } from "@ipare/http";
 import { MicroStartup } from "@ipare/micro";
-//{!env
+//{http
 import * as fs from "fs";
 import path from "path";
 //}
@@ -163,7 +163,7 @@ export default <T extends HttpStartup & MicroStartup>(startup: T) =>
     .useRouter();
 // }
 
-//{!env
+//{http
 const version = (() => {
   const pkgName = "package.json";
   let dir = __dirname;
