9a15aaa507bd8d6916e3980972b480d596bc21e1
hal-wang <hi@hal.wang>
2022-12-21 22:24:46 +0800

    chore: remove usless code

--- packages/cli/test/services/file.service.test.ts
+++ packages/cli/test/services/file.service.test.ts
@@ -1,26 +1,6 @@
 import { FileService } from "../../src/services/file.service";
 import { runTest } from "./runTest";
-import * as fs from "fs";
 import inquirer from "inquirer";
-import path from "path";
-
-runTest(FileService, async (ctx, service) => {
-  if (!fs.existsSync("dist")) {
-    fs.mkdirSync("dist");
-  }
-  const dir = path.join("dist", "file");
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
-
-  fs.writeFileSync(dir + "/f1.js", "f1");
-  fs.writeFileSync(dir + "/f2.ts", "f2");
-  service.globDelete(dir, "**/*.ts");
-  service.globDelete(dir + "not-exist", "**/*.ts");
-
-  expect(fs.existsSync(dir + "/f1.js")).toBeTruthy();
-  expect(fs.existsSync(dir + "/f2.ts")).toBeFalsy();
-});
 
 function testOverwrite(value: boolean) {
   runTest(FileService, async (ctx, service) => {
