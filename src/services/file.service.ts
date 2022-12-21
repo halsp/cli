1aba0088796ae0e77c0c61a1a3fd6890108331b8
hal-wang <hi@hal.wang>
2022-12-21 22:24:46 +0800

    chore: remove usless code

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,27 +1,8 @@
 import path from "path";
 import * as fs from "fs";
 import inquirer from "inquirer";
-import glob from "glob";
 
 export class FileService {
-  public globDelete(cwd: string, pattern: string, ignore: string[] = []) {
-    if (!fs.existsSync(cwd)) return;
-
-    const paths = glob
-      .sync(pattern, {
-        ignore,
-        cwd: cwd,
-      })
-      .reverse();
-    for (const p of paths) {
-      const completePath = path.join(cwd, p);
-      fs.rmSync(completePath, {
-        recursive: true,
-        force: true,
-      });
-    }
-  }
-
   public async isOverwrite(message: string): Promise<boolean> {
     const { overwrite } = await inquirer.prompt([
       {
