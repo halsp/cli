576f255e3b94009c1df94913c39cd25ce167e4c8
hal-wang <hi@hal.wang>
2022-12-26 15:49:00 +0800

    feat: add command serve

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Command } from "commander";
 import { BuildCommand } from "./commands/build.command";
 import { CreateCommand } from "./commands/create.command";
 import { InfoCommand } from "./commands/info.command";
+import { ServeCommand } from "./commands/serve.command";
 import { StartCommand } from "./commands/start.command";
 import { TemplateCommand } from "./commands/template.command";
 import { UpdateCommand } from "./commands/update.command";
@@ -19,6 +20,7 @@ new BuildCommand().register(program);
 new StartCommand().register(program);
 new InfoCommand().register(program);
 new UpdateCommand().register(program);
+new ServeCommand().register(program);
 
 program.parse(process.argv);
 
---
+++ src/middlewares/serve.middleware.ts
@@ -0,0 +1,55 @@
+import { Middleware } from "@ipare/core";
+import { Inject } from "@ipare/inject";
+import { CommandService } from "../services/command.service";
+
+export class ServeMiddleware extends Middleware {
+  @Inject
+  private readonly commandService!: CommandService;
+
+  private get targetPath() {
+    return this.ctx.commandArgs.targetPath;
+  }
+  private get port() {
+    return this.commandService.getOptionVlaue<string>("port");
+  }
+  private get hostname() {
+    return this.commandService.getOptionVlaue<string>("hostname");
+  }
+  private get hideDir() {
+    return this.commandService.getOptionVlaue<boolean>("hideDir");
+  }
+  private get exclude() {
+    const strs = this.commandService.getOptionVlaue<string>("exclude");
+    if (!strs) return undefined;
+
+    return strs.split(" ").filter((item) => !!item);
+  }
+  private get prefix() {
+    return this.commandService.getOptionVlaue<string>("prefix");
+  }
+  private get encoding() {
+    return this.commandService.getOptionVlaue<string>("encoding");
+  }
+
+  async invoke() {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { NativeStartup } = require("@ipare/native");
+    require("@ipare/static");
+
+    const listen = await new NativeStartup()
+      .useStatic({
+        dir: this.targetPath ?? process.cwd(),
+        listDir: !this.hideDir,
+        use404: true,
+        useIndex: true,
+        method: "ANY",
+        useExt: true,
+        exclude: this.exclude,
+        prefix: this.prefix,
+        encoding: this.encoding as BufferEncoding,
+      })
+      .dynamicListen(this.port, this.hostname);
+
+    this.ctx.res.setBody(listen);
+  }
+}
