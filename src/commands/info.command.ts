ee307459dcb2974aa7a9b1e80ce9e27599c3263e
hal-wang <hi@hal.wang>
2023-04-14 00:00:04 +0800

    feat(info): add app argument

--- src/commands/info.command.ts
+++ src/commands/info.command.ts
@@ -2,6 +2,7 @@ import { BaseCommand } from "./base.command";
 import { Command } from "commander";
 import { CliStartup } from "../cli-startup";
 import { InfoMiddleware } from "../middlewares/info.middleware";
+import { ChdirMiddleware } from "../middlewares/chdir.middleware";
 
 export class InfoCommand extends BaseCommand {
   register(command: Command): void {
@@ -9,11 +10,15 @@ export class InfoCommand extends BaseCommand {
       .command("info")
       .alias("i")
       .description("Display halsp project details")
+      .argument("[app]", "Where is the app")
       .setCommonOptions()
-      .action(async (command: Record<string, boolean | string>) => {
-        await new CliStartup("info", undefined, command)
-          .add(InfoMiddleware)
-          .run();
-      });
+      .action(
+        async (app: string, command: Record<string, boolean | string>) => {
+          await new CliStartup("info", { app }, command)
+            .add(ChdirMiddleware)
+            .add(InfoMiddleware)
+            .run();
+        }
+      );
   }
 }
