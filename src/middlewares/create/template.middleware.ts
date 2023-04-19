6e0cdffe6bfdf95c98ef46f15ba7df921219c0e2
hal-wang <hi@hal.wang>
2023-04-19 23:30:51 +0800

    feat(template): add hook

--- src/middlewares/create/template.middleware.ts
+++ src/middlewares/create/template.middleware.ts
@@ -1,15 +1,30 @@
 import { Middleware } from "@halsp/core";
 import { Inject } from "@halsp/inject";
-import path from "path";
+import { CommandService } from "../../services/command.service";
+import { CreateService } from "../../services/create.service";
+import { FileService } from "../../services/file.service";
 import { RunnerService } from "../../services/runner.service";
+import path from "path";
 import * as fs from "fs";
 import walk from "ignore-walk";
-import { FileService } from "../../services/file.service";
-import { CreateService } from "../../services/create.service";
 import glob from "glob";
-import { CommandService } from "../../services/command.service";
+import { ChalkService } from "../../services/chalk.service";
+
+type CopyConfig = {
+  template: string;
+  path?: string;
+  branch?: string;
+};
+
+type TemplateConfig = {
+  extends?: CopyConfig;
+  preCommand?: string;
+  postCommand?: string;
+};
 
 export class TemplateMiddleware extends Middleware {
+  @Inject
+  private readonly commandService!: CommandService;
   @Inject
   private readonly runnerService!: RunnerService;
   @Inject
@@ -17,67 +32,67 @@ export class TemplateMiddleware extends Middleware {
   @Inject
   private readonly createService!: CreateService;
   @Inject
-  private readonly commandService!: CommandService;
+  private readonly chalkService!: ChalkService;
 
-  get name() {
-    return this.ctx.commandArgs.name;
-  }
   private get targetDir() {
     return this.createService.targetDir;
   }
-  get template() {
-    return this.commandService.getOptionVlaue<string>("template")!;
-  }
-  get templateUrl() {
-    if (!this.template.includes("/")) {
-      return `https://github.com/halsp/template`;
-    }
-
-    if (this.template.startsWith("http")) {
-      return this.template;
-    }
-
-    return `https://github.com/${this.template}`;
-  }
-  get templateChildPath() {
-    if (!this.template.includes("/")) {
-      return this.template;
-    } else {
-      return this.commandService.getOptionVlaue<string>("path");
-    }
-  }
-  get nodeModulesPath() {
+  private get nodeModulesPath() {
     return path.resolve(__dirname, "../../../node_modules");
   }
-  get cacheDir() {
+  private get cacheDir() {
     return ".halsp-cli-template";
   }
-  get cacheDirPath() {
+  private get cacheDirPath() {
     return path.resolve(this.nodeModulesPath, this.cacheDir);
   }
-  get excludesFiles() {
-    return [".halspignore"];
+  private get excludesFiles() {
+    return [".halspignore", ".halsptemprc.json"];
   }
 
   async invoke() {
+    const copyResult = await this.copy({
+      template: this.commandService.getOptionVlaue<string>("template")!,
+      branch: this.commandService.getOptionVlaue<string>("branch"),
+      path: this.commandService.getOptionVlaue<string>("path"),
+    });
+    if (copyResult) {
+      await this.next();
+    }
+  }
+
+  async copy(config: CopyConfig): Promise<boolean> {
     await this.clean();
 
-    if (!this.cloneTemplate(this.templateUrl)) {
-      return;
+    if (
+      !this.cloneTemplate(this.getTemplateUrl(config.template), config.branch)
+    ) {
+      return false;
     }
 
     const templateDir = path.resolve(
       this.cacheDirPath,
-      this.templateChildPath ?? ""
+      this.getTemplateChildPath(config) ?? ""
     );
+    const templateConfig = await this.getTemprc(templateDir);
+    if (templateConfig.preCommand) {
+      console.log(
+        this.chalkService.blueBright(
+          `Execute preCommand of ${config.template}: ${templateConfig.preCommand}`
+        )
+      );
+      if (!this.runHook(templateDir, templateConfig.preCommand)) {
+        return false;
+      }
+    }
 
     let paths = await walk({
       path: templateDir,
       ignoreFiles: this.getIgnoreFiles(templateDir),
     });
-    const excludesFiles = await this.getExcludeFiles();
+    const excludesFiles = await this.getExcludeFiles(templateDir);
     paths = paths
-      .map((item) => item.replace(/\\/g, ""))
+      .map((item) => item.replace(/\\/g, "/"))
       .filter((p) => !excludesFiles.some((e) => e == p));
 
     for (const filePath of paths) {
@@ -87,9 +102,30 @@ export class TemplateMiddleware extends Middleware {
       await fs.promises.rename(sourceFile, targetFile);
     }
 
-    await this.clean();
+    if (templateConfig.postCommand) {
+      console.log(
+        this.chalkService.blueBright(
+          `Execute postCommand of ${config.template}: ${templateConfig.postCommand}`
+        )
+      );
+      if (!this.runHook(this.targetDir, templateConfig.postCommand)) {
+        return false;
+      }
+    }
 
-    await this.next();
+    if (templateConfig.extends) {
+      return await this.copy(templateConfig.extends);
+    }
+
+    await this.clean();
+    return true;
+  }
+
+  private runHook(templateDir: string, command: string) {
+    const commandRunResult = this.runnerService.run(command, undefined, {
+      cwd: templateDir,
+    });
+    return commandRunResult.status == 0;
   }
 
   private async clean() {
@@ -112,11 +148,11 @@ export class TemplateMiddleware extends Middleware {
     return result;
   }
 
-  private async getExcludeFiles() {
+  private async getExcludeFiles(dir: string) {
     const result: string[] = [];
     for (const excludes of this.excludesFiles) {
       const paths = await glob(excludes, {
-        cwd: this.cacheDirPath,
+        cwd: dir,
         dot: true,
         nodir: true,
       });
@@ -135,9 +171,7 @@ export class TemplateMiddleware extends Middleware {
     return undefined;
   }
 
-  private cloneTemplate(url: string) {
-    const branch = this.commandService.getOptionVlaue<string>("branch");
-
+  private cloneTemplate(url: string, branch?: string) {
     const args = ["clone", url, this.cacheDir];
     if (branch) {
       args.push("-b");
@@ -147,5 +181,34 @@ export class TemplateMiddleware extends Middleware {
       cwd: this.nodeModulesPath,
     });
     return cloneResult.status == 0;
+  }
+
+  private getTemplateChildPath(config: CopyConfig) {
+    if (!config.template.includes("/")) {
+      return config.template;
+    } else {
+      return config.path;
+    }
+  }
+
+  private getTemplateUrl(template: string) {
+    if (!template.includes("/")) {
+      return `https://github.com/halsp/template`;
+    }
+
+    if (template.startsWith("http")) {
+      return template;
+    }
+
+    return `https://github.com/${template}`;
+  }
+
+  private async getTemprc(dir: string): Promise<TemplateConfig> {
+    const filePath = path.join(dir, ".halsptemprc.json");
+    if (fs.existsSync(filePath)) {
+      return JSON.parse(await fs.promises.readFile(filePath, "utf-8"));
+    } else {
+      return {};
+    }
   }
 }
