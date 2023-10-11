8f261b05fc55d4e4c969d28ab32847d774edf9c7
hal-wang <hi@hal.wang>
2023-10-11 23:55:16 +0800

    chore: remove minimal

--- scripts/create-halsp.ts
+++ scripts/create-halsp.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import prettier from "prettier";
 
-function editPackage() {
+async function editPackage() {
   const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
   pkg.name = "create-halsp";
   pkg.description = "Halsp 快速开始脚手架工具，由 @halsp/cli 自动生成";
@@ -11,31 +11,33 @@ function editPackage() {
   delete pkg.bin["halsp"];
   pkg.bin["create-halsp"] = "bin/create-halsp.js";
 
-  fs.writeFileSync(
+  await fs.promises.writeFile(
     "./package.json",
-    prettier.format(JSON.stringify(pkg), {
+    await prettier.format(JSON.stringify(pkg), {
       parser: "json",
-    })
+    }),
   );
 }
 
-function editTsconfig() {
+async function editTsconfig() {
   const tsconfig = JSON.parse(fs.readFileSync("./tsconfig.json", "utf-8"));
   tsconfig.include = ["src/create-halsp.ts"];
 
-  fs.writeFileSync(
+  await fs.promises.writeFile(
     "./tsconfig.json",
-    prettier.format(JSON.stringify(tsconfig), {
+    await prettier.format(JSON.stringify(tsconfig), {
       parser: "json",
-    })
+    }),
   );
 }
 
-function editReadme() {
+async function editReadme() {
   const readme = fs.readFileSync("./create-halsp.README.md", "utf-8");
-  fs.writeFileSync("./README.md", readme);
+  await fs.promises.writeFile("./README.md", readme);
 }
 
-editTsconfig();
-editPackage();
-editReadme();
+(async () => {
+  await editTsconfig();
+  await editPackage();
+  await editReadme();
+})();
