ca361b5d4b80df3fe31c1fedc5e6fd04dafc4937
hal-wang <hi@hal.wang>
2024-04-17 20:52:47 +0800

    test: revert

--- src/services/build.services/config.service.ts
+++ src/services/build.services/config.service.ts
@@ -1,4 +1,4 @@
-import { Context, isUndefined, safeImport } from "@halsp/core";
+import { Context, isUndefined } from "@halsp/core";
 import path from "path";
 import type { Configuration, ConfigEnv } from "../../configuration";
 import { Inject } from "@halsp/inject";
@@ -6,6 +6,7 @@ import { CommandService } from "../command.service";
 import { FileService } from "../file.service";
 import * as fs from "fs";
 import { DepsService } from "../deps.service";
+import { pathToFileURL } from "url";
 import { TsconfigService } from "./tsconfig.service";
 import ts from "typescript";
 import {
@@ -178,10 +179,15 @@ export class ConfigService {
   }
 
   private async importJsConfig(configFilePath: string) {
-    const module = await safeImport(configFilePath);
+    let module: any;
+    if (this.isConfigEsm) {
+      module = await import(pathToFileURL(configFilePath).toString());
+    } else {
+      module = _require(configFilePath);
+    }
     if (typeof module == "function") {
       return module(this.configEnv);
-    } else if (module?.default) {
+    } else if (module.default) {
       return module.default(this.configEnv);
     } else {
       return {};
